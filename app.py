9b506f5dd4d06bda011a3e1fc070ea40aa7f9a7f
ks-1220 <yeahkriti30@gmail.com>
2025-03-20 20:04:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-#import shap
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
